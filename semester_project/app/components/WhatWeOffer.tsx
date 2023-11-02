a99acb78d06884f911df0d5d2c19d20beb976f46
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2023-11-02 20:39:41 +0100

    apostrophes replaced by &apos;

--- semester_project/app/components/WhatWeOffer.tsx
+++ semester_project/app/components/WhatWeOffer.tsx
@@ -6,16 +6,16 @@ export default function WhatWeOffer(){
             <Image  src="/images/split.jpg" width={800} height={200}  alt="slika"/>
             <div className="bg-white  w-1/2 grid grid-cols-2 grid-rows-2 ">
                 <div className="inline-grid">
-                    <p className="bg-slate-200 p-14 text-center font-serif font-medium not-italic tracking-wide">Explore&apos; our varied accommodations, from cozy rooms in Split's historic center to stunning sea-view apartments, catering to every traveler's preference.</p>
+                    <p className="bg-slate-200 p-14 text-center font-serif font-medium not-italic tracking-wide">Explore our varied accommodations, from cozy rooms in Split&apos;s historic center to stunning sea-view apartments, catering to every traveler&apos;s preference.</p>
                 </div>
                 <div className="inline-grid">
-                    <p className="bg-slate-300 p-14 text-center font-serif font-medium not-italic tracking-wide">Experience&apos; the charm of Split's old town or the allure of its seaside views with our diverse location options.</p>
+                    <p className="bg-slate-300 p-14 text-center font-serif font-medium not-italic tracking-wide">Experience the charm of Split&apos;s old town or the allure of its seaside views with our diverse location options.</p>
                 </div>
                 <div className="inline-grid">
-                    <p className="bg-slate-400 p-14 text-center font-serif font-medium not-italic tracking-wide">Elegant&apos; furnishings, fully equipped kitchens, and spacious terraces are just a glimpse of the exceptional amenities we provide for your perfect stay.</p>
+                    <p className="bg-slate-400 p-14 text-center font-serif font-medium not-italic tracking-wide">Elegant furnishings, fully equipped kitchens, and spacious terraces are just a glimpse of the exceptional amenities we provide for your perfect stay.</p>
                 </div>
                 <div className="inline-grid">
-                    <p className="bg-slate-500 p-14 text-center font-serif font-medium not-italic tracking-wide">Discover&apos; the best of the area with our personalized recommendations, enhancing your stay with exclusive local insights we offer.</p>
+                    <p className="bg-slate-500 p-14 text-center font-serif font-medium not-italic tracking-wide">Discover the best of the area with our personalized recommendations, enhancing your stay with exclusive local insights we offer.</p>
                 </div>
             </div>
         </div>
