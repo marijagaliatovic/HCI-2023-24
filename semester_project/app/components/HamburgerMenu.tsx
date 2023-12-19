3efe1f0428d269cc63a9a202f4c314e047d3ba3d
karladelic <kdelic01@fesb.hr>
2023-12-19 20:08:41 +0100

    added links

--- semester_project/app/components/HamburgerMenu.tsx
+++ semester_project/app/components/HamburgerMenu.tsx
@@ -51,10 +51,10 @@ export default function HamburgerMenu({ open, clickHandler }: HamburgerProps) {
           <a onClick={handleAccomodationClick}>Accomodation</a>
           {isAccomodationOpen && (
             <ul className="absolute z-10 left-0 mt-2 py-2 w-48 bg-white rounded-md shadow-lg">
-              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/">Apartment Listings</Link></li>
-              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/">Special Offers</Link></li>
-              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/">Amenities</Link></li>
-              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/">Reviews</Link></li>
+              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/apartmentlistings">Apartment Listings</Link></li>
+              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/specialoffers">Special Offers</Link></li>
+              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/amenities">Amenities</Link></li>
+              <li className="block px-4 py-2 text-sm font-semibold hover:underline"><Link href="/reviews">Reviews</Link></li>
             </ul>
           )}
         </li>
