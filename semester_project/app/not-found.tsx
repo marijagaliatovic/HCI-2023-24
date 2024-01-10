d9a3f98a27d5c5149c50e5d0d62b0e3fb12f6a92
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-01-11 00:01:56 +0100

    not found page

--- semester_project/app/not-found.tsx
+++ semester_project/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound(){
         <main className="text-center m-32 z-50 relative ">
             <h2 className="text-3xl font-bold">Oh, sh!t</h2>
             <p className="font-medium text-lg">We could not find the page you were looking for.</p>
-            <p className="font-medium text-lg">Go back to the <Link href="/" className="font-bold underline">Home</Link>!</p>
+            <p className="font-medium text-lg">Go back <Link href="/" className="font-bold underline">Home</Link>!</p>
         </main>
         
     )
