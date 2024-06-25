8026bf0d49eba193ae0b52349373feade8d1b100
karladelic <kdelic01@fesb.hr>
2024-06-25 11:08:34 +0200

    fixed loading cms data

--- semester_project/app/reviews/page.tsx
+++ semester_project/app/reviews/page.tsx
@@ -1,18 +1,33 @@
+"use client";
 import Footer from "../components/Footer";
 import ReviewItem from "../components/ReviewItem";
 import getAllReviews from "@/lib/contentfulAllReviews";
+import { useEffect, useState } from "react";
+import { reviewsItem } from "@/lib/contentfulAllReviews";
 
+const Reviews = () => {
+    const [reviews, setReviews] = useState<reviewsItem[]>([]);
 
-const Reviews = async () => {
-    const reviews = await getAllReviews();
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await getAllReviews();
+          setReviews(data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+      fetchData();
+    }, []);
+    
 
     return(
       <>
         <div className="flex flex-col items-center relative lg:mt-12 py-8">
             <h2 className="font-bold text-xl lg:text-2xl self-center mt-10 top-24">Reviews</h2>
             <div className="small-line"></div>
-            <div className="flex flex-col lg:flex-row flex-wrap items-center justify-center top-8 lg:top-10 relative lg:mx-20 lg:items-stretch">
-              {reviews.map((item,index)=>(
+            <div className="flex flex-col lg:flex-row flex-wrap items-center  justify-center top-8 lg:top-10 relative lg:mx-20 lg:items-stretch ">
+              {reviews.map((item, index)=>(
                 <ReviewItem key={index} {...item} />
               ))}
              </div>
@@ -20,6 +35,6 @@ const Reviews = async () => {
         <Footer/>
       </>
     );
-}
+};
 
 export default Reviews;
