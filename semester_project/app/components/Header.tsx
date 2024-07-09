61bbc8e71c4c3f6df13cf59489e04d7b669d4216
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-07-09 22:26:59 +0200

    removed colors

--- semester_project/app/components/Header.tsx
+++ semester_project/app/components/Header.tsx
@@ -5,7 +5,6 @@ import React, { useState } from "react";
 
 export default function Header() {
   const [isAccomodationOpen, setIsAccommodationOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState<number>(0);
 
   const handleAccommodationHover = () => {
     setIsAccommodationOpen(true);
@@ -15,19 +14,14 @@ export default function Header() {
     setIsAccommodationOpen(false);
   };
 
-  const handleAccommodationClick = (item:number) =>{
+  const handleAccommodationClick = () =>{
     setIsAccommodationOpen(false);
-    setActiveItem(item);
-  };
-
-  const handleButtonClick = (item:number) => {
-    setActiveItem(item);
   };
 
   return (
     <nav className="hidden fixed w-full z-10 top-0 lg:block">
       <ul className="flex flex-row justify-center gap-6 pt-10 pl-20 pr-20 pb-5">
-        <li className={`text-base font-semibold not-italic hover:underline  cursor-pointer bg-transparent ${activeItem === 0 ? "text-red-700": "text-stone-700"}`}><Link className="bg-transparent" href="/" onClick={()=>handleButtonClick(0)}>HOME</Link></li>
+        <li className="text-base font-semibold not-italic hover:underline  cursor-pointer bg-transparent"><Link className="bg-transparent" href="/">HOME</Link></li>
 
         <li className="text-base font-semibold not-italic relative  cursor-pointer bg-transparent" 
           onMouseEnter={handleAccommodationHover}
@@ -40,24 +34,24 @@ export default function Header() {
           {isAccomodationOpen && (
             <ul className="absolute z-10 left-1/2 transform -translate-x-1/2 p-2 w-52 bg-gray-400 rounded-md shadow-lg">
               <li className="bg-transparent">
-                <Link href="/apartmentlistings" className="bg-gray-400 block px-4 py-2 text-sm font-semibold hover:bg-slate-500 transition duration-300 ease-in-out rounded-md " onClick={()=>handleAccommodationClick(3)}>APARTMENT LISTINGS</Link>
+                <Link href="/apartmentlistings" className="bg-gray-400 block px-4 py-2 text-sm font-semibold hover:bg-slate-500 transition duration-300 ease-in-out rounded-md " onClick={handleAccommodationClick}>APARTMENT LISTINGS</Link>
               </li>
               <li className="bg-transparent">
-                <Link href="/specialoffers" className="bg-gray-400 block px-4 py-2 text-sm font-semibold hover:bg-slate-500 transition duration-300 ease-in-out rounded-md" onClick={()=>handleAccommodationClick(3)}>SPECIAL OFFERS</Link>
+                <Link href="/specialoffers" className="bg-gray-400 block px-4 py-2 text-sm font-semibold hover:bg-slate-500 transition duration-300 ease-in-out rounded-md" onClick={handleAccommodationClick}>SPECIAL OFFERS</Link>
               </li>
               <li className="bg-transparent">
-                <Link href="/reviews" className="bg-gray-400 block px-4 py-2 text-sm font-semibold hover:bg-slate-500 transition duration-300 ease-in-out rounded-md" onClick={()=>handleAccommodationClick(3)}>REVIEWS</Link>
+                <Link href="/reviews" className="bg-gray-400 block px-4 py-2 text-sm font-semibold hover:bg-slate-500 transition duration-300 ease-in-out rounded-md" onClick={handleAccommodationClick}>REVIEWS</Link>
               </li>
             </ul>
           )}
         </li>
         
-        <li className = {`text-base font-semibold not-italic hover:underline  cursor-pointer bg-transparent ${activeItem === 1 ? "text-red-700": "text-stone-700"}`}>
-          <Link className = "bg-transparent" href="/contact" onClick={()=>handleButtonClick(1)}>CONTACT US</Link>
+        <li className = "text-base font-semibold not-italic hover:underline  cursor-pointer bg-transparent">
+          <Link className = "bg-transparent" href="/contact" >CONTACT US</Link>
         </li>
 
-        <li className = {`text-base font-semibold not-italic hover:underline  cursor-pointer bg-transparent ${activeItem === 2 ? "text-red-700": "text-stone-700"}`}>
-          <Link className="bg-transparent" href="/signup" onClick={()=>handleButtonClick(2)}>SIGN UP</Link>
+        <li className = "text-base font-semibold not-italic hover:underline  cursor-pointer bg-transparent ">
+          <Link className="bg-transparent" href="/signup">SIGN UP</Link>
         </li>
       </ul>
     </nav>
